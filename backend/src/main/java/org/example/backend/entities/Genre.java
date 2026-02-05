18e6476f9f0280dd1c2beceb8cc31ce0f1762817
Sanira Deneth <sanira.adesha@gmail.com>
2026-02-05 16:13:51 +0530

    created Entities and there relationships

---
+++ backend/src/main/java/org/example/backend/entities/Genre.java
@@ -0,0 +1,28 @@
+package org.example.backend.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "genres")
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "genres")
+    private Set<User> users = new HashSet<>();
+}
---
+++ backend/src/main/java/org/example/backend/entities/User.java
@@ -0,0 +1,58 @@
+package org.example.backend.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "business_name")
+    private String businessName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToOne(mappedBy = "user")
+    private Reservation reservation;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_genres",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id")
+    )
+    private Set<Genre> genres = new HashSet<>();
+    public void addGenre(Genre genre) {genres.add(genre);};
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
