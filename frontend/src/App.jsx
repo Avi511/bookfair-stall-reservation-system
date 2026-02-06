892c8b9ee2767665043c31935b03052b0e13d561
Avishka Medagamagodage <aimedagamagodage2003@gmail.com>
2026-02-06 12:30:09 +0530

    feat: add Navbar and Footer components, update styles and dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Navbar from './components/layout/Navbar/Navbar';
-import Footer from './components/layout/Footer/Footer';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
