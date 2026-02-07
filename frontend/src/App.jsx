10091251262413020b9a694729731cae85c0cdb0
Dinith Pamunuwatta <155564826+dinithanki@users.noreply.github.com>
2026-02-08 00:02:51 +0530

    feat: add ScrollToTop component to reset scroll position on route changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,12 @@ import MessageBar from "./components/layout/MessageBar";
 import { ownerMessages } from "./data/ownerMessages";
 import Footer from "./components/layout/Footer";
 import AppRoutes from "./routes/AppRoutes";
+import ScrollToTop from "./routes/ScrollToTop";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
+      <ScrollToTop smooth />
       <MessageBar messages={ownerMessages} />
       <Navbar />
       <main className="flex-grow">
