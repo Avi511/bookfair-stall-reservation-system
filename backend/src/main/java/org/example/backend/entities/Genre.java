2df9592bc6678480829e5ede3de0f26b18cd17b2
Sanira Deneth <sanira.adesha@gmail.com>
2026-02-07 14:12:20 +0530

    Created Genre Controller

--- backend/src/main/java/org/example/backend/entities/Genre.java
+++ backend/src/main/java/org/example/backend/entities/Genre.java
@@ -18,11 +18,8 @@ public class Genre {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Integer id;
 
     @Column(name = "name")
     private String name;
-
-    @ManyToMany(mappedBy = "genres")
-    private Set<User> users = new HashSet<>();
 }
