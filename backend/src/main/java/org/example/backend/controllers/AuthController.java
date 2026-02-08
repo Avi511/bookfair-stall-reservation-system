06c06bd9a58298bdcf563ba6d08b088d1b111428
Sanira Deneth <sanira.adesha@gmail.com>
2026-02-08 16:01:47 +0530

    Added jwt token service and moved sensitive data to .env file

--- backend/src/main/java/org/example/backend/controllers/AuthController.java
+++ backend/src/main/java/org/example/backend/controllers/AuthController.java
@@ -3,7 +3,9 @@ package org.example.backend.controllers;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.example.backend.dtos.JwtResponse;
 import org.example.backend.dtos.LoginRequest;
+import org.example.backend.services.JwtService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,9 +19,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(
+    public ResponseEntity<JwtResponse> login(
             @Valid @RequestBody LoginRequest request
     ) {
            authenticationManager.authenticate(
@@ -28,8 +31,9 @@ public class AuthController {
                            request.getPassword()
                    )
            );
+           var token = jwtService.generatedToken(request.getEmail());
 
-            return ResponseEntity.ok().build();
+            return ResponseEntity.ok(new JwtResponse(token));
     }
 
     @ExceptionHandler(BadCredentialsException.class)
