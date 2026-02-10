7a46f9a6cca7e4f4b41bda8f696c85c1e0a187c2
Dinith Pamunuwatta <dinithchamo@gmail.com>
2026-02-10 19:55:38 +0530

    feat(event): add update event request model

---
+++ backend/src/main/java/org/example/backend/dtos/UpdateEventRequest.java
@@ -0,0 +1,27 @@
+package org.example.backend.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class UpdateEventRequest {
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @Positive
+    private Integer year;
+
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date startDate;
+
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date endDate;
+}
