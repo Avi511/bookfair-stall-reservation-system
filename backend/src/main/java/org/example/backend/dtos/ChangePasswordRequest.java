857a3bf5b79940ae6929449e02e3b6df1ba961d7
kavidu717 <dushmanthakavidu143@gmail.com>
2026-02-11 08:45:22 +0530

    fixed dtos

--- backend/src/main/java/org/example/backend/dtos/ChangePasswordRequest.java
+++ backend/src/main/java/org/example/backend/dtos/ChangePasswordRequest.java
@@ -1,16 +1,16 @@
 package org.example.backend.dtos;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class ChangePasswordRequest {
-    @NotBlank(message = "Old password is required.")
-    @Size(min = 6, max = 25, message = "Password must be between 6 to 25 characters long.")
+
+    @NotBlank
     private String oldPassword;
 
-    @NotBlank(message = "New password is required.")
-    @Size(min = 6, max = 25, message = "Password must be between 6 to 25 characters long.")
+    @NotBlank
     private String newPassword;
 }
