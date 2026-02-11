0fa5fc7bfe6e4f976f99164070b41657479edba4
kavidu717 <dushmanthakavidu143@gmail.com>
2026-02-11 09:14:34 +0530

    change the id type and create  the create the EventRepository in proper place

--- backend/src/main/java/org/example/backend/repositories/EventRepository.java
+++ backend/src/main/java/org/example/backend/repositories/EventRepository.java
@@ -1,4 +1,7 @@
 package org.example.backend.repositories;
 
-public class EventRepository {
+import org.example.backend.entities.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
 }
--- backend/src/main/java/org/example/backend/services/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.backend.services;
-
-import org.example.backend.entities.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Integer> {
-}
