69e33f4f529bc89aa164bd490f506b5020406a9e
Dinith Pamunuwatta <155564826+dinithanki@users.noreply.github.com>
2026-02-13 21:03:02 +0530

    feat: wrap App component with AuthProvider for authentication context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,22 @@ import { ownerMessages } from "./data/ownerMessages";
 import Footer from "./components/layout/Footer";
 import AppRoutes from "./routes/AppRoutes";
 import ScrollToTop from "./routes/ScrollToTop";
+import { AuthProvider } from "./auth/AuthContext";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <ScrollToTop smooth />
-      <MessageBar messages={ownerMessages} />
-      <Navbar />
-      <Toaster position="top-right" />
-      <main className="flex-grow">
-        <AppRoutes />
-      </main>
-      <Footer />
-    </div>
+    <AuthProvider>
+      <div className="flex flex-col min-h-screen">
+        <ScrollToTop smooth />
+        <MessageBar messages={ownerMessages} />
+        <Navbar />
+        <Toaster position="top-right" />
+        <main className="flex-grow">
+          <AppRoutes />
+        </main>
+        <Footer />
+      </div>
+    </AuthProvider>
   );
 }
 
